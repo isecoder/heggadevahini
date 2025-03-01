2e9addee7c621c5418131f62216e1109b03368b8
Shreekara B <shreekarab5@gmail.com>
2025-03-01 11:10:37 +0530

    Navbar

--- components/navbar/nav.tsx
+++ components/navbar/nav.tsx
@@ -8,17 +8,17 @@ const Navbar = () => {
 
   return (
     <>
-    
+      {/* Desktop Navbar */}
       <nav className="hidden md:flex items-center space-x-12 font-bold -ml-36">
-        <Link href="/" className="text-black hover:text-[#F48634]">Home</Link>
-        <Link href="/about" className="text-black hover:text-[#F48634]">About us</Link>
-        <Link href="/e-paper" className="text-black hover:text-[#F48634]">E-Paper</Link>
-        <Link href="/latest" className="text-black hover:text-[#F48634]">Latest News</Link>
-        <Link href="/subscribe" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>Subscribe</Link>
-        <Link href="/contact" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>Contact Us</Link>
-        </nav>
+        <Link href="/" className="text-black hover:text-[#F48634]">ಮುಖಪುಟ</Link>
+        <Link href="/about" className="text-black hover:text-[#F48634]">ನಮ್ಮ ಬಗ್ಗೆ</Link>
+        <Link href="/e-paper" className="text-black hover:text-[#F48634]">ಇ-ಪೇಪರ್</Link>
+        <Link href="/latest" className="text-black hover:text-[#F48634]">ತಾಜಾ ಸುದ್ದಿ</Link>
+        <Link href="/subscribe" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ಚಂದಾದಾರರಾಗಿ</Link>
+        <Link href="/contact" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ಸಂಪರ್ಕಿಸಿ</Link>
+      </nav>
 
-     
+      {/* Mobile Menu Button */}
       <button 
         className="md:hidden block text-black" 
         onClick={() => setIsOpen(!isOpen)}
@@ -26,7 +26,7 @@ const Navbar = () => {
         {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
       </button>
 
-      
+      {/* Mobile Sidebar */}
       <div className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transition-transform transform ${isOpen ? "translate-x-0" : "translate-x-full"} z-50`}>
         <div className="flex justify-end p-4">
           <button onClick={() => setIsOpen(false)}>
@@ -34,16 +34,16 @@ const Navbar = () => {
           </button>
         </div>
         <nav className="flex flex-col space-y-6 font-bold text-lg p-6">
-        <Link href="/" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>Home</Link>
-          <Link href="/about" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>About us</Link>
-          <Link href="/e-paper" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>E-Paper</Link>
-          <Link href="/latest" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>Latest News</Link>
-          <Link href="/subscribe" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>Subscribe</Link>
-          <Link href="/contact" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>Contact Us</Link>
+          <Link href="/" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ಮುಖಪುಟ</Link>
+          <Link href="/about" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ನಮ್ಮ ಬಗ್ಗೆ</Link>
+          <Link href="/e-paper" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ಇ-ಪೇಪರ್</Link>
+          <Link href="/latest" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ತಾಜಾ ಸುದ್ದಿ</Link>
+          <Link href="/subscribe" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ಚಂದಾದಾರರಾಗಿ</Link>
+          <Link href="/contact" className="text-black hover:text-[#F48634]" onClick={() => setIsOpen(false)}>ಸಂಪರ್ಕಿಸಿ</Link>
         </nav>
       </div>
 
-     
+      {/* Overlay when menu is open */}
       {isOpen && <div className="fixed inset-0 bg-black bg-opacity-50 z-40" onClick={() => setIsOpen(false)}></div>}
     </>
   );
